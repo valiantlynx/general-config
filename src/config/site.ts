7742e2d66926c9718b157b55c0bc00af3e30c489
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2024-12-19 21:22:51 +0100

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const site: SiteConfig = {
 	noImage: '/assets/no-image.png',
 	title: 'valiantlynx',
 	subtitle: 'Where Imagination Meets Innovation',
-	pocketbase: 'https://whalescans.fly.dev',
+	pocketbase: 'https://pb.valiantlynx.com',
 	lang: 'en-US',
 	description:
 		'Multi-Disciplinary Engineer: Exploring the Intersection of AI, Blockchain, Web Development, and Product Design',
